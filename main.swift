715964eade40a81484db1b4ddedf91d41e0c0635
Aadish Samir Samir <aadish.samir@giis.edu.sg>
2020-12-22 04:43:54 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,6 @@
+//This piece of code is about myself
+print("Hello.")
+print("My name is Aadish.")
+print("I am eight years old.")
+print("I study in Global Indian International School Punggol, Singapore.")
+print("My class is in PYP 3B.")
